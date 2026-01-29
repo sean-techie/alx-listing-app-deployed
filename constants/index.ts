fb04863e35ece0fb787a76adc6c32221beae292a
Reseanne Rampou <reseanner@gmail.com>
2026-01-29 17:43:04 +0200

    Update index.ts

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,21 @@
-import type { PropertyProps } from "../types/property";
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
 
 export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
   {
